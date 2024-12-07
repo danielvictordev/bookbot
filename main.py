6f2a73cf409339d94465044317ce67d00c81e3d0
Daniel Victor <174794152+danielvictordev@users.noreply.github.com>
2024-12-07 09:12:05 +0530

    Count characters in book

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = 'books/frankenstein.txt'
     book = read(book_path)
     word_count = count_words(book)
-    print(word_count)
+    character_count = count_character(book)
+    print(character_count)
 
 
 def read(path):
@@ -12,6 +13,13 @@ def read(path):
 
 def count_words(text):
     return len(text.split())
+
+
+def count_character(string):
+    character_count = {}
+    for c in string.lower():
+        character_count[c] = character_count.get(c, 0) + 1
+    return character_count
 
 
 if __name__ == '__main__':
