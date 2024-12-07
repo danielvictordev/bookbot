197bc7fbfbe575845688a22d7994e911b9ade9b8
Daniel Victor <174794152+danielvictordev@users.noreply.github.com>
2024-12-07 08:59:54 +0530

    Count words in book

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 def main():
     book_path = 'books/frankenstein.txt'
     book = read(book_path)
-    print(book)
+    word_count = count_words(book)
+    print(word_count)
 
 
 def read(path):
     with open(path, 'r', encoding='utf8') as f:
         return f.read()
+
+
+def count_words(text):
+    return len(text.split())
 
 
 if __name__ == '__main__':
